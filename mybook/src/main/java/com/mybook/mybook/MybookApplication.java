e929d7eaa7723d6f187330cc435755ffaf3c24ed
zongzf <1548285635@qq.com>
2018-11-30 15:08:46 +0800

    for test

---
+++ mybook/src/main/java/com/mybook/mybook/MybookApplication.java
@@ -0,0 +1,12 @@
+package com.mybook.mybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybookApplication.class, args);
+    }
+}
